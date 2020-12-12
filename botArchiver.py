4bac8921216315d0c3beb60fd51a4c66f71a953a
Smertin Nikita <smertin-nikita@mail.ru>
2020-12-12 18:38:30 +0500

    Register vk command

--- botArchiver.py
+++ botArchiver.py
@@ -1,33 +1,41 @@
+import config
 import telebot
+from telebot import types
+from VkRequester import VkUser
 
-import os
-import os.path as op
-
-
-TOKEN_PATH = 'config'
+bot = telebot.TeleBot(config.telegram_token, parse_mode=None)
 
 
-def get_token():
-    path = op.join(os.getcwd(), TOKEN_PATH)
-    try:
-        with open(path, 'r') as f:
-            token = f.readline()
-    except FileNotFoundError:
-        print("File not found")
-    return token
-
-
-bot = telebot.TeleBot(get_token(), parse_mode=None)
+def generate_markup():
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.KeyboardButton('Да')
+    itembtn2 = types.KeyboardButton('Нет')
+    markup.add(itembtn1, itembtn2)
+    return markup
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Ты Какашка!")
+    bot.reply_to(message, "Ваш id или screen name в Вконтакте?")
+
+
+@bot.message_handler(commands=['vk'])
+def vk_auth(message):
+    command = message.text.split(maxsplit=1)
+    if len(command) < 1:
+        check_answer(message)
+    else:
+        uid = command[1].rsplit()
+        vk_user = VkUser(uid)
 
 
 @bot.message_handler(content_types=['text'])
-def echo_all(message):
-    bot.send_message(message.chat.id, 'Ты какашка!')
+def check_answer(message):
+    text = """Привет,
+    Введи /vk <страница vk>;
+    Введи /disk 
+    """
+    bot.reply_to(message, text)
 
 
 bot.polling()
